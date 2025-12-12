44ac088e568810c469b8c9d6e95f7346efdc23eb
P1yansh <piyanshshukla7824@gmail.com>
2025-12-12 13:56:56 +0530

    Revert "Merge pull request #6 from rizzesh/main"
    This reverts commit d55d0d54515e7f809d022a8ad7f4c7bce880f05d, reversing
    changes made to 48739fc89eae74e3d6c2f909950bace14fbcde64.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import Menu from './components/Menu';
 import Dashboard from './components/Dashboard';
 import Checkout from './components/Checkout';
 import CustomerReviews from './components/CustomerReviews';
-import About from "./components/About";
-import Contact from "./components/Contact";
 
 
 function App() {
@@ -47,10 +45,6 @@ function App() {
           <Route path="/checkout" element={<Checkout user={user} />} />
 
           <Route path="/reviews" element={<CustomerReviews />} />
-          
-          <Route path="/about" element={<About />} />
-           
-          <Route path="/contact" element={<Contact />} />
 
         </Routes>
       </div>
--- src/components/CustomerReviews.jsx
+++ src/components/CustomerReviews.jsx
@@ -1,6 +1,4 @@
-// import React, { useEffect, useState } from "react"
-// ;
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function CustomerReviews() {
   const [name, setName] = useState("");
